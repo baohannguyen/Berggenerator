8bc3f29b714ef39c834efb5891be678413ee9538
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2022-11-28 00:59:07 +0100

    added delete code and second color ramp for roughness

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 import bpy
 import mathutils
 
+bpy.ops.object.select_all(action='SELECT') 
+bpy.ops.object.delete(use_global=False, confirm=False)
+bpy.ops.outliner.orphans_purge()
+
 bpy.context.scene.render.engine = 'CYCLES'
 bpy.context.scene.cycles.device = 'GPU'
 
 mountainSize = 5
-#bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
 
 bpy.ops.mesh.primitive_plane_add(size = mountainSize, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
 bpy.ops.object.editmode_toggle()
@@ -16,19 +20,37 @@ mat = bpy.data.materials.new(name="mountainTexture")
 mat.use_nodes = True
 mat.cycles.displacement_method = 'DISPLACEMENT'
 allNodes = mat.node_tree.nodes
+
 #Code für die Farbe
 #mat.node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.0143394, 0.207794, 0.8, 1)
 disNode = allNodes.new(type="ShaderNodeDisplacement")
 material_output = allNodes.get("Material Output")
-
 textImage = allNodes.new(type="ShaderNodeTexImage")
-textImage.image =bpy.data.images.load(filepath="C:\\Users\\ducha\\OneDrive\\Desktop\\Screenshot 2022-11-20 114700.png")
+cRamp = allNodes.new(type="ShaderNodeValToRGB")
+colorRamp = allNodes.new(type="ShaderNodeValToRGB")
+
+textImage.image =bpy.data.images.load(filepath="C:\\Users\\ducha\\OneDrive\\Dokumente\\GitHub\\Berggenerator\\irland_height_map.png")
 textImage.image.colorspace_settings.name = 'Linear'
+
 mat.node_tree.links.new(disNode.outputs[0], material_output.inputs[2])
 mat.node_tree.links.new(textImage.outputs[0], disNode.inputs[0])
+mat.node_tree.links.new(textImage.outputs[0], cRamp.inputs[0])
+mat.node_tree.links.new(textImage.outputs[0], colorRamp.inputs[0])
+mat.node_tree.links.new(cRamp.outputs[0], allNodes["Principled BSDF"].inputs[0])
+mat.node_tree.links.new(colorRamp.outputs[0], allNodes["Principled BSDF"].inputs[9])
+
+cRamp.color_ramp.elements[0].position = (0.500)
+cRamp.color_ramp.elements[0].color = (0.33,0.1,0,1)
+cRamp.color_ramp.elements[1].position = (0.520)
+
+colorRamp.color_ramp.elements[0].position = (0.027)
+colorRamp.color_ramp.elements[1].position = (0.482)
+
+
 disNode.inputs[2].default_value = 0.3
 bpy.context.active_object.data.materials.append(mat)
 bpy.ops.object.editmode_toggle()
+
 
 
 
