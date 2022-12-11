b3c6153fa5e2b83af5dceb1ef832e26929980042
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2022-12-11 20:53:53 +0100

    Update script.py

--- script.py
+++ script.py
@@ -9,18 +9,23 @@ bpy.context.scene.render.engine = 'CYCLES'
 bpy.context.scene.cycles.device = 'GPU'
 
 
-mountainSize = 5
+mountainSize = 2
 bpy.ops.object.light_add(type='SUN', radius=1, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
 
+
 bpy.ops.mesh.primitive_plane_add(size = mountainSize, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+obj = bpy.context.active_object
+
 bpy.ops.object.editmode_toggle()
 bpy.ops.mesh.subdivide(number_cuts=100)
 bpy.ops.mesh.subdivide(number_cuts=1)
 
+obj.scale = (5, 5, 5)
 mat = bpy.data.materials.new(name="mountainTexture")
 mat.use_nodes = True
 mat.cycles.displacement_method = 'DISPLACEMENT'
 allNodes = mat.node_tree.nodes
+
 
 #Code für die Farbe
 #mat.node_tree.nodes["Principled BSDF"].inputs[0].default_value = (0.0143394, 0.207794, 0.8, 1)
@@ -30,13 +35,18 @@ textImage = allNodes.new(type="ShaderNodeTexImage")
 cRamp = allNodes.new(type="ShaderNodeValToRGB")
 colorRamp = allNodes.new(type="ShaderNodeValToRGB")
 
-textImage.image =bpy.data.images.load(filepath="C:\\Users\\Anwender\\Desktop\\datenverarbeitung\\Berggenerator\\berg.jpg")
+textImage.image =bpy.data.images.load(filepath="C:\\Users\\ducha\\OneDrive\\Dokumente\\GitHub\\Berggenerator\\ruap.png")
 textImage.image.colorspace_settings.name = 'Linear'
+heightTexture = bpy.data.textures.new('textureMountain', type ='IMAGE')
+heightTexture.image = textImage.image
+dispMod = obj.modifiers.new("Displace", type='DISPLACE')
+dispMod.texture = heightTexture
+
 
 mat.node_tree.links.new(disNode.outputs[0], material_output.inputs[2])
-mat.node_tree.links.new(textImage.outputs[0], disNode.inputs[0])
-mat.node_tree.links.new(textImage.outputs[0], cRamp.inputs[0])
-mat.node_tree.links.new(textImage.outputs[0], colorRamp.inputs[0])
+#mat.node_tree.links.new(textImage.outputs[0], disNode.inputs[0])
+#mat.node_tree.links.new(textImage.outputs[0], cRamp.inputs[0])
+#mat.node_tree.links.new(textImage.outputs[0], colorRamp.inputs[0])
 mat.node_tree.links.new(cRamp.outputs[0], allNodes["Principled BSDF"].inputs[0])
 mat.node_tree.links.new(colorRamp.outputs[0], allNodes["Principled BSDF"].inputs[9])
 
@@ -51,15 +61,16 @@ colorRamp.color_ramp.elements[1].position = (0.482)
 disNode.inputs[2].default_value = 3.3
 disNode.inputs[1].default_value = 0
 
-bpy.context.active_object.data.materials.append(mat)
+obj.modifiers.new("ps", type='PARTICLE_SYSTEM')
+ps= obj.particle_systems[0].settings
+ps.type ='HAIR'
+ps.use_advanced_hair = True
+ps.render_type = 'COLLECTION'
+bpy.ops.import_scene.obj(filepath="C:\\Users\\ducha\\OneDrive\\Dokumente\\GitHub\\Berggenerator\\smalltree.obj")
+bpy.ops.import_scene.obj(filepath="C:\\Users\\ducha\\OneDrive\\Dokumente\\GitHub\\Berggenerator\\smalltree2.obj")
 
+bpy.context.active_object.data.materials.append(mat)
 
 #bpy.ops.object.shade_smooth()
 
 bpy.ops.object.editmode_toggle()
-
-
-
-
-
-
